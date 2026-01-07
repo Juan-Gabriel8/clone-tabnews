bfd73961ff50fec2a288af9bcd525f9b2d918d20
Juan Gabriel <65375925+Juan-Gabriel8@users.noreply.github.com>
2026-01-07 01:17:27 +0000

    feat: home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() { 
-    return <h1>Tab News</h1>
+    return <div>
+        <title>Tab news</title>
+        <h1>Pagina inicial do tab news!</h1>
+    </div>
 }
 
 export default Home;
