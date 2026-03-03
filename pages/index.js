81d4dc14d07b1beb5db9574e4f05f8d7da774d7e
Juan-Gabriel8 <juan.gabriel.m710@gmail.com>
2026-03-03 13:51:15 -0300

    Home teste

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,56 @@
+import Trash from "../assets/Trash.svg";
+
 function Home() {
+  const users = [
+    {
+      id: "123423",
+      name: "Juan Gabriel",
+      age: 23,
+      email: "juan.teste@email.com",
+    },
+    {
+      id: "98745398",
+      name: "Camila Cardoso",
+      age: 22,
+      email: "camila.teste@email.com",
+    },
+    {
+      id: "98745398asd",
+      name: "Kalleb Cardoso",
+      age: 2,
+      email: "kalleb.teste@email.com",
+    },
+  ];
+
   return (
     <div>
       <title>Tab news</title>
-      <h1>Pagina inicial do tab news!</h1>
+      <form>
+        <h1>Pagina inicial do tab news!</h1>
+        <input name="Nome" placeholder="Nome" type="text" />
+        <input name="Idade" placeholder="Idade" type="number" />
+        <input name="Email" placeholder="Email" type="email" />
+        <button type="button">Cadastrar</button>
+      </form>
+
+      {users.map((user) => (
+        <div key={user.id}>
+          <div>
+            <p>
+              Nome: <span>{user.name}</span>
+            </p>
+            <p>
+              Idade: <span>{user.age}</span>
+            </p>
+            <p>
+              Email: <span>{user.email}</span>
+            </p>
+          </div>
+          <button>
+            <img src={Trash} alt="Lixeira" />
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
